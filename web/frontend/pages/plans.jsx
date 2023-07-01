47b0f3f346634c1bbb2317a5234dc2933647e490
Joshua Gosse <jlgosse@gmail.com>
2023-07-01 15:29:06 +0200

    Attempt to subscribe to plan

--- web/frontend/pages/plans.jsx
+++ web/frontend/pages/plans.jsx
@@ -2,9 +2,18 @@ import { HorizontalGrid, Page } from "@shopify/polaris";
 import { useMantle } from "../mantle/MantleProvider";
 import { Card } from "../mantle/Card";
 import { PlanCard } from "../mantle/PlanCard";
+import { useContext } from "react";
+import { Context } from "@shopify/app-bridge-react";
+import { Redirect } from "@shopify/app-bridge/actions";
+
+const remoteRedirect = (url, app) => {
+  const redirect = Redirect.create(app);
+  return redirect.dispatch(Redirect.Action.REMOTE, url);
+};
 
 export default function PlansPage() {
-  const { plans, currentSubscription, subscribe } = useMantle();
+  const app = useContext(Context);
+  const { plans, currentSubscription, subscribe, refresh } = useMantle();
 
   return (
     <Page title="Plans">
@@ -20,7 +29,12 @@ export default function PlansPage() {
               plan={plan}
               currentSubscription={currentSubscription}
               onSubscribe={async (_plan) => {
-                await subscribe(_plan.id);
+                const subscription = await subscribe(_plan.id, "/settings");
+                if (subscription.confirmationUrl) {
+                  remoteRedirect(subscription.confirmationUrl, app);
+                } else {
+                  await refresh();
+                }
               }}
             />
           ))}
