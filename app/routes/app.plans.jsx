5de999e841e82d95ccd0fcb6bb870d1c7ac197ce
Joshua Gosse <jlgosse@gmail.com>
2024-03-24 09:17:24 -0600

    bump package

--- app/routes/app.plans.jsx
+++ app/routes/app.plans.jsx
@@ -1,21 +1,21 @@
 import { Layout, Page } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
-import { useMantle, HorizontalCards } from "@heymantle/surface";
+import { useMantle, HorizontalPlanCards } from "@heymantle/surface";
 
 export default function PlansPage() {
-  const { customer, plans, subscribe, cancelSubscription } = useMantle();
+  const { customer, plans, mantleClient } = useMantle();
 
   return (
     <Page>
       <TitleBar title="Select a plan" />
       <Layout>
         <Layout.Section>
-          <HorizontalCards
+          <HorizontalPlanCards
             customer={customer}
             plans={plans}
-            onSubscribe={async ({ planId, discountId }) => {
+            onSubscribe={async (plan) => {
               const returnUrl = '/app/plans';
-              const subscription = await subscribe({ planId, discountId, returnUrl });
+              const subscription = await mantleClient.subscribe({ planId: plan.id, returnUrl });
               const url = subscription.confirmationUrl;
               open(url, "_top");
             }}
