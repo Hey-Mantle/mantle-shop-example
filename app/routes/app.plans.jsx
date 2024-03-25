43d080f0a9ca25c3415f91a33ca22ad96516bfab
Joshua Gosse <jlgosse@gmail.com>
2024-03-25 07:24:17 -0600

    Fix plan page

--- app/routes/app.plans.jsx
+++ app/routes/app.plans.jsx
@@ -1,24 +1,37 @@
 import { Layout, Page } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
-import { useMantle, HorizontalPlanCards } from "@heymantle/surface";
+import { useMantle, PlanCardStack, PlanCardType } from "@heymantle/surface";
+import { useState } from "react";
 
 export default function PlansPage() {
   const { customer, plans, mantleClient } = useMantle();
+  const [loading, setLoading] = useState(false);
+
+  const handleSelectPlan = async ({ plan, discount }) => {
+    console.log(plan, discount);
+    setLoading(true);
+    console.log('mantleClient: ', mantleClient);
+    const subscription = await mantleClient.subscribe({
+      planId: plan.id,
+      discountId: discount?.id,
+      returnUrl: '/app/plans'
+    });
+    const url = subscription.confirmationUrl;
+    console.log('url: url');
+    open(url, "_top");
+    setLoading(false);
+  }
 
   return (
     <Page>
       <TitleBar title="Select a plan" />
       <Layout>
         <Layout.Section>
-          <HorizontalPlanCards
+          <PlanCardStack
+            cardType={PlanCardType.Highlighted}
             customer={customer}
             plans={plans}
-            onSubscribe={async (plan) => {
-              const returnUrl = '/app/plans';
-              const subscription = await mantleClient.subscribe({ planId: plan.id, returnUrl });
-              const url = subscription.confirmationUrl;
-              open(url, "_top");
-            }}
+            onSelectPlan={handleSelectPlan}
           />
         </Layout.Section>
       </Layout>
