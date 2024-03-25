33b60bee933fc92886280777bd9c829dfa51caff
Joshua Gosse <jlgosse@gmail.com>
2024-03-25 07:25:41 -0600

    Remove loading for now

--- app/routes/app.plans.jsx
+++ app/routes/app.plans.jsx
@@ -1,26 +1,19 @@
 import { Layout, Page } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useMantle, PlanCardStack, PlanCardType } from "@heymantle/surface";
-import { useState } from "react";
 
 export default function PlansPage() {
   const { customer, plans, mantleClient } = useMantle();
-  const [loading, setLoading] = useState(false);
 
   const handleSelectPlan = async ({ plan, discount }) => {
-    console.log(plan, discount);
-    setLoading(true);
-    console.log('mantleClient: ', mantleClient);
     const subscription = await mantleClient.subscribe({
       planId: plan.id,
       discountId: discount?.id,
-      returnUrl: '/app/plans'
+      returnUrl: "/app/plans",
     });
     const url = subscription.confirmationUrl;
-    console.log('url: url');
     open(url, "_top");
-    setLoading(false);
-  }
+  };
 
   return (
     <Page>
